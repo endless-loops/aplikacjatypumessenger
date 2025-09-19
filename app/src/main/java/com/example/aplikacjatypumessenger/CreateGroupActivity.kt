7f1d15a21ab02a6418c1188eff2c75b2a1d2f53a
endless-loops <most.critical.point@gmail.com>
2025-09-19 12:21:37 +0200

    nieco poprawione

--- app/src/main/java/com/example/aplikacjatypumessenger/CreateGroupActivity.kt
+++ app/src/main/java/com/example/aplikacjatypumessenger/CreateGroupActivity.kt
@@ -11,6 +11,7 @@ import com.example.aplikacjatypumessenger.models.Chat
 import com.example.aplikacjatypumessenger.models.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
--- app/src/main/java/com/example/aplikacjatypumessenger/GroupManagementActivity.kt
+++ app/src/main/java/com/example/aplikacjatypumessenger/GroupManagementActivity.kt
@@ -12,6 +12,7 @@ import com.example.aplikacjatypumessenger.models.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
